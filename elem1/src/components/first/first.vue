d250b895084959c66ae85e306f5c6e434fb9f1ee
lanouhn <>
2018-05-03 22:33:40 +0800

    11

---
+++ elem1/src/components/first/first.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+    <div class="head">
+      <div>ele.me</div>
+      <div>
+        <router-link to="/">登录</router-link>
+        <span>|</span>
+        <router-link to="/">注册</router-link>
+      </div>
+    </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "first"
+    }
+</script>
+
+<style scoped>
+.head{
+  width: 100%;
+  height: 45.7px;
+  background:#3190e8;
+}
+</style>
