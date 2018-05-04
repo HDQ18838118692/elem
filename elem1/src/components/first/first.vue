bda5a503554247716f4b632166f20fff8ee0204b
lanouhn <>
2018-05-04 13:57:07 +0800

    222

--- elem1/src/components/first/first.vue
+++ elem1/src/components/first/first.vue
@@ -17,13 +17,31 @@
         定位不准时，请在城市列表中选择
       </div>
     </div>
+        <div>
+<router-link to="/">
+  郑州
+</router-link>
+        </div>
       </div>
     </div>
 </template>
 
 <script>
+  import Vue from "vue"
+  var api1="http://cangdu.org:8001/v1/pois"
     export default {
-        name: "first"
+        name: "first",
+      data(){
+          return{
+
+          }
+      },
+      created(){
+        Vue.axios.get(api1).then((response) => {
+          console.log(response.data)
+
+        })
+      }
     }
 </script>
 
